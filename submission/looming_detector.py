df3f39c0fe51b816a1cbf152f8fd462b198cf083
Niel Mistry <niel@nielmistry.com>
2025-05-18 13:05:09 +0200

    flyvis dependency

--- submission/looming_detector.py
+++ submission/looming_detector.py
@@ -9,7 +9,10 @@ from copy import deepcopy
 from flygym.vision import Retina
 from PIL import Image
 import numba as nb
-import time
+import flyvis
+from flygym.examples.vision import RealTimeVisionNetworkView, RetinaMapper
+from torch import Tensor
+from flyvis.utils.activity_utils import LayerActivity
 
 
 class RawVideoHandler:
@@ -93,66 +96,53 @@ class RawVideoHandler:
         self.flat_frame = None
 
 
-class EMDDirection(IntEnum):
-    UP = 1
-    DOWN = 2
-    LEFT = 3
-    RIGHT = 4
-
-
-class RectificationType(IntEnum):
-    ON = 1
-    OFF = 2
-
-
-class PathwayIndices(IntEnum):
-    HORIZ_LR = 0
-    HORIZ_RL = 1
-    VERT_UD = 2
-    VERT_DU = 3
-
-
 class LoomDetector:
     def __init__(self, input_size: tuple, debug=False):
-        TS = 0.01
-        HPF_TAU = 0.300
-        HPF_CORNER = 1 / (2 * np.pi * HPF_TAU)
-        BUF_SIZE = 10
+        self.vision_results_dir = flyvis.results_dir / "flow/0000/000"
+        self.vision_network_view = RealTimeVisionNetworkView(self.vision_results_dir)
+        self.vision_network = self.vision_network_view.init_network(chkpt="best_chkpt")
+        self.vision_retina_mapper = RetinaMapper()
 
-        LFP_TAU = 0.05
-        LPF_CORNER = 1 / (2 * np.pi * LFP_TAU)
+        self.vision_refresh_rate = 1 / 0.01
 
-        self.frames_recvd = 0
-        self.logger = logging.getLogger("ld")
-
-        self.frame_buffer = deque(
-            [np.zeros(input_size) for _ in range(BUF_SIZE)], BUF_SIZE
-        )
-
-        self.emd_signals = {dir: 0 for dir in EMDDirection}
-        self.hpf_b, self.hpf_a = scipy.signal.butter(
-            1, HPF_CORNER, "high", output="ba", fs=1 / TS
-        )
-
-        self.lpf_b, self.lpf_a = scipy.signal.butter(
-            1, LPF_CORNER, "low", output="ba", fs=1 / TS
-        )
-
-        self.hpf_history = deque([np.zeros(input_size)], 2)
-        self.lpf_on_history = deque([(np.zeros(input_size), np.zeros(input_size))], 2)
-        self.lpf_off_history = deque([(np.zeros(input_size), np.zeros(input_size))], 2)
-
-        self.initialized = False
-        self.retina = Retina()
+        self._vision_network_initialized = False
+        self._vision_nn_buf = None
+        self._vision_nn_arr_buf = None
 
         self.debug = debug
 
-        self.kernels = {}
-        for dir in EMDDirection:
-            self.kernels[dir] = self.create_kernel(dir)
+    # Thanks to flygym/examples/vision/realistic_vision.py for the infra code :)
+    def _init_network(self, vision_obs: np.ndarray):
+        vision_obs_grayscale = vision_obs.max(axis=-1)
+        visual_input = self.vision_retina_mapper.flygym_to_flyvis(vision_obs_grayscale)
+        visual_input = Tensor(visual_input).to(flyvis.device)
+        initial_state = self.vision_network.fade_in_state(
+            t_fade_in=1.0,
+            dt=1 / self.vision_refresh_rate,
+            initial_frames=visual_input.unsqueeze(1),
+        )
+        self.vision_network.setup_step_by_step_simulation(
+            dt=1 / self.vision_refresh_rate,
+            initial_state=initial_state,
+            as_states=False,
+            num_samples=2,
+        )
+        self._initial_state = initial_state
+        self._vision_network_initialized = True
 
-        if debug:
-            self.rvh = RawVideoHandler("hpf", self.retina)
+    def _get_visual_nn_activities(self, vision_obs):
+        vision_obs_grayscale = vision_obs.max(axis=-1)
+        visual_input = self.vision_retina_mapper.flygym_to_flyvis(vision_obs_grayscale)
+        visual_input = Tensor(visual_input).to(flyvis.device)
+        nn_activities_arr = self.vision_network.forward_one_step(visual_input)
+        nn_activities_arr = nn_activities_arr.cpu().numpy()
+        nn_activities = LayerActivity(
+            nn_activities_arr,
+            self.vision_network.connectome,
+            keepref=True,
+            use_central=False,
+        )
+        return nn_activities, nn_activities_arr
 
     def __enter__(self):
         if self.debug:
@@ -162,200 +152,19 @@ class LoomDetector:
         if self.debug:
             self.rvh.__exit__(exc_type, exc_value, traceback)
 
-    def kernel_to_image(self, kernel: np.ndarray) -> np.ndarray:
-        img = np.zeros((2, 721, 2))
-        img[0, :, 0] = kernel
-        img[1, :, 0] = kernel
-        return img
+    def process(self, obs: dict, info: dict):
+        if "vision" not in obs:
+            return
 
-    def create_kernel(
-        self, direction: EMDDirection, off_dir_start=0.333, off_dir_end=0.666
-    ) -> np.ndarray:
-        image = np.zeros(721)
+        if not self._vision_network_initialized:
+            self._init_network(obs["vision"])
 
-        id_map = self.retina.ommatidia_id_map
-        id_map_shape_y, id_map_shape_x = id_map.shape
-
-        off_dir_x_range = (
-            int(off_dir_start * id_map_shape_x),
-            int(off_dir_end * id_map_shape_x),
-        )
-        off_dir_y_range = (
-            int(off_dir_start * id_map_shape_y),
-            int(off_dir_end * id_map_shape_y),
-        )
-
-        if direction == EMDDirection.UP:
-            ids = np.unique(
-                id_map[
-                    : id_map_shape_y // 2, off_dir_x_range[0] : off_dir_x_range[1]
-                ].ravel()
+        if info["vision_updated"] or self._vision_nn_buf is None:
+            nn_activities, nn_activities_arr = self._get_visual_nn_activities(
+                obs["vision"]
             )
-        elif direction == EMDDirection.LEFT:
-            ids = np.unique(
-                id_map[
-                    off_dir_y_range[0] : off_dir_y_range[1], : id_map_shape_x // 2
-                ].ravel()
-            )
-        elif direction == EMDDirection.RIGHT:
-            ids = np.unique(
-                id_map[
-                    off_dir_y_range[0] : off_dir_y_range[1], id_map_shape_x // 2 :
-                ].ravel()
-            )
-        elif direction == EMDDirection.DOWN:
-            ids = np.unique(
-                id_map[
-                    id_map_shape_y // 2 :, off_dir_x_range[0] : off_dir_x_range[1]
-                ].ravel()
-            )
-
-        ids = ids - 1
-        image[ids] = 1
-        return image
-
-    def edge_rectification(
-        self, input: np.ndarray, threshold: float, type: RectificationType
-    ):
-        input = input.copy()
-        pre_shape = input.shape
-        flattened = input.ravel()
-
-        if type == RectificationType.ON:
-            pattern = flattened > threshold
-        elif type == RectificationType.OFF:
-            pattern = flattened < threshold
-
-        flattened[pattern] = 10 * np.abs(flattened[pattern] - threshold)
-        flattened[~pattern] = np.zeros(np.count_nonzero(~pattern))
-
-        return flattened.reshape(pre_shape)
-
-    @staticmethod
-    @nb.njit(parallel=True)
-    def make_motion_detector_images(id_map, on, off, lpf_on, lpf_off):
-        px_y, px_x = id_map.shape
-
-        on_path = np.zeros((px_y, px_x, 4, 2))
-        off_path = np.zeros((px_y, px_x, 4, 2))
-
-        for side in nb.prange(2):
-            for y in nb.prange(px_y - 1):
-                for x in nb.prange(px_x - 1):
-                    id = id_map[y, x]
-                    id_right = id_map[y, x + 1]
-                    id_down = id_map[y + 1, x]
-
-                    if id < 0 or id_right < 0 or id_down < 0:
-                        continue
-
-                    on_path[y, x, PathwayIndices.HORIZ_LR.value, side] = (
-                        lpf_on[side, id, 0] * on[side, id_right, 0]
-                    )
-
-                    on_path[y, x, PathwayIndices.HORIZ_RL.value, side] = (
-                        on[side, id, 0] * lpf_on[side, id_right, 0]
-                    )
-
-                    on_path[y, x, PathwayIndices.VERT_UD.value, side] = (
-                        lpf_on[side, id, 0] * on[side, id_down, 0]
-                    )
-                    on_path[y, x, PathwayIndices.VERT_DU.value, side] = (
-                        on[side, id, 0] * lpf_on[side, id_down, 0]
-                    )
-
-                    off_path[y, x, PathwayIndices.HORIZ_LR.value, side] = (
-                        lpf_off[side, id, 0] * off[side, id_right, 0]
-                    )
-
-                    off_path[y, x, PathwayIndices.HORIZ_RL.value, side] = (
-                        off[side, id, 0] * lpf_off[side, id_right, 0]
-                    )
-
-                    off_path[y, x, PathwayIndices.VERT_UD.value, side] = (
-                        lpf_off[side, id, 0] * off[side, id_down, 0]
-                    )
-                    off_path[y, x, PathwayIndices.VERT_DU.value, side] = (
-                        off[side, id, 0] * lpf_off[side, id_down, 0]
-                    )
-
-        return on_path, off_path
-
-    def process(self, images: np.ndarray) -> float:
-        PROCESS_PERIOD = 1
-        EMD_OFFSET = 5
-
-        self.frames_recvd += 1
-
-        images[:, :, 0] = (
-            images[:, :, 0] + images[:, :, 1]
-        )  # combine the channels since they are useless to us.
-
-        if self.frames_recvd < self.frame_buffer.maxlen:
-            # just add the frame to the buffer
-            self.frame_buffer.appendleft(images)
-            return 0
-
-        self.frame_buffer.appendleft(images)
-
-        hpf = (
-            self.hpf_b[0] * (images)
-            + self.hpf_b[1] * self.frame_buffer[1]
-            - self.hpf_a[1] * self.hpf_history[0]
-        ) / self.hpf_a[0]
-
-        # first order hpf in pipeline
-        self.hpf_history.appendleft(hpf)
-
-        on_rect = self.edge_rectification(hpf, 0.0, RectificationType.ON)
-        off_rect = self.edge_rectification(hpf, 0.05, RectificationType.OFF)
-
-        on_last, lpf_on_last = self.lpf_on_history[0]
-        off_last, lpf_off_last = self.lpf_off_history[0]
-
-        lpf_on = (
-            self.lpf_b[0] * on_rect
-            + self.lpf_b[1] * on_last
-            - self.lpf_a[1] * lpf_on_last
-        ) / self.lpf_a[0]
-        lpf_off = (
-            self.lpf_b[0] * off_rect
-            + self.lpf_b[1] * off_last
-            - self.lpf_a[1] * lpf_off_last
-        ) / self.lpf_a[0]
-
-        self.lpf_on_history.appendleft((on_rect, lpf_on))
-        self.lpf_off_history.appendleft((off_rect, lpf_off))
-
-        combined = on_rect + off_rect
-
-        on_path, off_path = self.make_motion_detector_images(
-            self.retina.ommatidia_id_map, on_rect, off_rect, lpf_on, lpf_off
-        )
-        on_path = 255 * (off_path / off_path.max())
-
-        if self.frames_recvd % 50 == 0:
-            print("Saving...")
-            np.save(f".stuff/on_path_{self.frames_recvd}", on_path)
-
-        if self.debug:
-            self.rvh.add_frame(self.frame_buffer[0])
-            self.rvh.add_frame(self.hpf_history[0])
-            self.rvh.add_frame(on_rect)
-            self.rvh.add_frame(lpf_on)
-            self.rvh.add_frame(off_rect)
-            self.rvh.add_frame(lpf_off)
-
-            self.rvh.add_frame(on_path[:, :, 0, 0], "flat")
-            self.rvh.add_frame(on_path[:, :, 1, 0], "flat")
-            self.rvh.add_frame(on_path[:, :, 2, 0], "flat")
-            self.rvh.add_frame(on_path[:, :, 3, 0], "flat")
-
-            self.rvh.commit_frame()
-
-        return combined
-        # else:
-        #     pass # maybe log
+            self._vision_nn_buf = nn_activities
+            self._vision_nn_arr_buf = nn_activities_arr
 
 
 def generate_visual_pattern(
@@ -464,6 +273,7 @@ if __name__ == "__main__":
         help="Path to the pkl file to process. Default is .stuff/incoming_ball.pkl.",
     )
     args = parser.parse_args()
+    ld = LoomDetector((2, 721, 2), debug=False)
 
     if args.command == "generator":
         pattern = args.pattern
@@ -476,7 +286,6 @@ if __name__ == "__main__":
             print("Loaded existing data")
         else:
             print("Generating new data")
-            ld = LoomDetector((2, 721, 2), debug=True)
             views = generate_visual_pattern(ld, pattern, scan_frames, num_scans)
             views = [view[1] for view in views]
 
@@ -490,12 +299,15 @@ if __name__ == "__main__":
             data = pickle.load(f)
         views = data["vision"]
 
-    ld = LoomDetector((2, 721, 2), debug=True)
+    print(views[0])
 
-    length = len(views)
+    obs_arr = [{"vision": arr} for arr in views]
+    info_arr = [{"vision_updated": True} for _ in range(len(obs_arr))]
+
+    length = len(obs_arr)
     i = 1
     with ld:
-        for fly_view in views:
+        for i in range(length):
             print(f"{i}/{length}")
             i += 1
-            ld.process(fly_view)
+            ld.process(obs_arr[i], info_arr[i])
