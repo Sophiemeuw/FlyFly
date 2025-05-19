626a3348304f5e43eeaa05673b7c8474f595517c
Niel Mistry <niel@nielmistry.com>
2025-05-19 22:19:02 +0200

    add save to explore levels

--- explore_levels.py
+++ explore_levels.py
@@ -23,10 +23,16 @@ VISUALISATION_MODE = WITH_FLY_VISION
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Run the fly simulation.")
     parser.add_argument(
-        "--level", type=int, default=4, help="Level to load (default: -1)"
+        "--level", type=int, default=1, help="Level to load (default: -1)"
     )
     parser.add_argument(
         "--seed", type=int, default=0, help="Seed for the simulation (default: 0)"
+    )
+    parser.add_argument(
+        "--pickle",
+        type=str,
+        default="",
+        help="Path to save the visual sequences as a pickle",
     )
     args = parser.parse_args()
 
@@ -35,7 +41,7 @@ if __name__ == "__main__":
     timestep = 1e-4
 
     # you can pass in parameters to enable different senses here
-    fly = CobarFly(debug=False, enable_vision=True, render_raw_vision=False)
+    fly = CobarFly(debug=False, enable_vision=True, render_raw_vision=True)
 
     if level <= -1:
         level_arena = FlatTerrain()
@@ -71,11 +77,15 @@ if __name__ == "__main__":
     cv2.namedWindow("Simulation", cv2.WINDOW_NORMAL)
 
     with tqdm.tqdm(desc="running simulation") as progress_bar:
+        itr = 0
+        t = []
+
         while True:
             # Get observations
             obs, reward, terminated, truncated, info = sim.step(
                 controller.get_actions(obs)
             )
+            itr += 1
             if controller.done_level(obs):
                 # finish the path integration level
                 break
@@ -85,6 +95,10 @@ if __name__ == "__main__":
                     del obs["vision"]
                 if "raw_vision" in obs:
                     del obs["raw_vision"]
+                if "raw_vision" in info:
+                    del info["raw_vision"]
+            else:
+                t.append(itr)
             obs_hist.append(obs)
             info_hist.append(info)
 
@@ -92,11 +106,15 @@ if __name__ == "__main__":
             if rendered_img is not None:
                 if VISUALISATION_MODE == WITH_FLY_VISION:
                     rendered_img = render_image_with_vision(
-                        rendered_img, get_fly_vision(fly), obs["odor_intensity"],
+                        rendered_img,
+                        get_fly_vision(fly),
+                        obs["odor_intensity"],
                     )
                 elif VISUALISATION_MODE == WITH_RAW_VISION:
                     rendered_img = render_image_with_vision(
-                        rendered_img, get_fly_vision_raw(fly), obs["odor_intensity"],
+                        rendered_img,
+                        get_fly_vision_raw(fly),
+                        obs["odor_intensity"],
                     )
                 rendered_img = cv2.cvtColor(rendered_img, cv2.COLOR_BGR2RGB)
                 cv2.imshow("Simulation", rendered_img)
@@ -112,6 +130,27 @@ if __name__ == "__main__":
             progress_bar.update()
 
     print("Simulation finished")
+
+    pickle_save_path = args.pickle
+    if pickle_save_path:
+        import os
+        import pickle
+
+        video_frames = {
+            "t": t,
+            "raw_vision": [
+                hist_entry["raw_vision"]
+                for hist_entry in obs_hist
+                if "raw_vision" in hist_entry
+            ],
+            "vision": [
+                hist_entry["vision"]
+                for hist_entry in obs_hist
+                if "vision" in hist_entry
+            ],
+        }
+        with open(f"{pickle_save_path}.pkl", "wb") as f:
+            pickle.dump(video_frames, f)
 
     # Save video
     cam.save_video("./outputs/hybrid_controller.mp4", 0)
